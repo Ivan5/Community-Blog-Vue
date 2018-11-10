31914cac464723dd676b309c9e652d371e4e8e36
Ivan5 <converse_ivan@hotmail.com>
2018-11-09 23:08:28 -0600

    Add styles and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Navbar />
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+
+*{
+  font-family: 'Poppins', sans-serif;
+  border-radius: 0px !important;
+}
+
+body{
+  background-color: #f5f3ef;
 }
 </style>
