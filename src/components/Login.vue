0530698feb3f35aa5aa7f6bb6efac176f033adbb
Ivan5 <converse_ivan@hotmail.com>
2018-11-09 23:28:55 -0600

    implemenst signup component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,22 @@
 <template lang="html">
-<h1>Login</h1>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Login</h3>
+          <div class="form-group">
+            <input type="text" placeholder="Emial" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button type="submit" class="btn btn-success btn-block">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,9 +1,50 @@
 <template lang="html">
-<h1>Signup</h1>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Signup</h3>
+          <div class="form-group">
+            <input type="text" placeholder="Username" class="form-control" v-model="name">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Emial" class="form-control" v-model="email">
+          </div>
+          <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control" v-model="password">
+          </div>
+          <div class="form-group text-center">
+            <button type="submit" class="btn btn-success btn-block" @click="registerUser()">Signup</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data(){
+    return {
+      name:'',
+      email:'',
+      password:''
+    }
+  },
+  methods:{
+    registerUser(){
+      axios.post('https://react-blog-api.bahdcasts.com/api/auth/register', {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      }).then(response => {
+        console.log(response);
+      }).catch((response) => {
+        console.log(response);
+      })
+    }
+  }
 }
 </script>
 
