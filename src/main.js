d92a73c1ad177450dfe3cb58b7de390c0d237d11
Ivan5 <converse_ivan@hotmail.com>
2018-11-11 18:14:54 -0600

    Add Disqus in article component with vue-disqus

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueNoty from 'vuejs-noty'
+import VueDisqus from 'vue-disqus'
 import 'vuejs-noty/dist/vuejs-noty.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueNoty)
-
+Vue.use(VueDisqus)
 const authData = localStorage.getItem("auth");
 
 new Vue({
